419e3e1c5c74ecc234eb46225389afe387c9fa0b
HanwenXuTHU <hw-xu16@mails.tsinghua.edu.cn>
2021-11-08 22:03:37 +0800

    zero_shot_setting

--- zero_shot_setting/ProTranlator_main.py
+++ zero_shot_setting/ProTranlator_main.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from model import deepZSLModel, deepSDNModel
+from model import ProTranslatorModel
 from options import model_config, data_loading
 from file_loader import FileLoader
 from torch.utils.data import DataLoader
@@ -8,28 +8,30 @@ from utils import init_weights, EarlyStopping, compute_roc, vec2classes, compute
 import utils
 from tqdm import tqdm
 import numpy as np
-import gc
 
 
-class DeepZSL:
+class ProTranslator:
 
     def __init__(self, model_config):
         self.loss_func = torch.nn.BCELoss()
-        self.model = deepSDNModel.deepSDNModel(input_nc=model_config.input_nc,
+        self.model = ProTranslatorModel.deepSDNModel(input_nc=model_config.input_nc,
                                                 in_nc=model_config.in_nc,
                                                 max_kernels=model_config.max_kernels,
                                                 hidden_dim=model_config.hidden_dim,
                                                 feature=model_config.features,
                                                 seqL=model_config.max_len,
                                                 emb_dim=model_config.emb_dim)
+
         if len(model_config.gpu_ids) > 0:
             init_weights(self.model, init_type='xavier')
             self.model = self.model.cuda()
+
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=model_config.lr)
 
     def backward_model(self, input_seq, input_description, input_vector, emb_tensor, label):
         preds = self.model(input_seq, input_description, input_vector, emb_tensor)
         self.loss = self.loss_func(preds, label)
+
         self.optimizer.zero_grad()
         self.loss.backward()
         self.optimizer.step()
@@ -37,18 +39,19 @@ class DeepZSL:
 
 def main():
     data_opt = data_loading(dataset='goa_human_cat')
-    print('dataset is {}'.format(data_opt.dataset))
     model_opt = model_config()
     file = FileLoader(data_opt)
+
+    print('dataset is {}'.format(data_opt.dataset))
     print('loading data finished')
     feature_symb = '_'.join(model_opt.features)
-    logger = utils.get_logger(data_opt.dataset +
-                              data_opt.logger_name.format(feature_symb))
+    logger = utils.get_logger(data_opt.dataset + data_opt.logger_name.format(feature_symb))
+
     for fold_i in [0, 1, 2]:
         model_opt.emb_dim = file.emb_tensor_train.size(1)
         model_opt.vector_dim = np.size(list(file.prot_vector.values())[0].reshape(-1), 0)
         model_opt.description_dim = np.size(list(file.prot_description.values())[0].reshape(-1), 0)
-        model_predict = DeepZSL(model_opt)
+        model_predict = ProTranslator(model_opt)
         train_dataset = DataLoader(file.fold_training[fold_i], batch_size=model_opt.batch_size, shuffle=True)
         inference_dataset = DataLoader(file.fold_validation[fold_i], batch_size=model_opt.batch_size, shuffle=True)
         logger.info('fold : {}'.format(fold_i))
@@ -56,15 +59,18 @@ def main():
             train_loss, inference_loss = 0, 0
             num = 0
             print('Training iters')
+
             for j, train_D in tqdm(enumerate(train_dataset)):
                 model_predict.backward_model(train_D['seq'], train_D['description'], train_D['vector'], file.emb_tensor_train, train_D['label'])
                 train_loss += float(model_predict.loss.cpu())
                 num += 1
             train_loss = train_loss/num
+
             num = 0
             if i > -1:
                 print('inferring iters')
                 inference_preds, inference_label = [], []
+
                 for j, inference_D in tqdm(enumerate(inference_dataset)):
                     with torch.no_grad():
                         preds = model_predict.model(inference_D['seq'], inference_D['description'], inference_D['vector'], file.emb_tensor_train)
@@ -80,6 +86,7 @@ def main():
                         inference_label.append(label_inf)
                         inference_loss += float(loss.item())
                         num += 1
+
                 inference_loss = inference_loss/num
                 inference_preds, inference_label = torch.cat(tuple(inference_preds), dim=0), torch.cat(tuple(inference_label), dim=0)
                 inference_preds, inference_label = inference_preds.cpu().float().numpy(), inference_label.cpu().float().numpy()
@@ -101,6 +108,7 @@ def main():
                             auroc_percentage[T] += 100.0/len(file.fold_zero_shot_terms_list[fold_i])
                 for T in auroc_percentage.keys():
                     logger.info('fold:{} iter:{} roc auc:{} percentage:{}%'.format(fold_i, i, T, auroc_percentage[T]))
+
         torch.save(model_predict, model_opt.save_path.format(data_opt.dataset, fold_i, feature_symb))
 
 
